fce78daf1780f89c3a1819be4d53fa08b04b9cf1
Marvin Heilemann <11534760+muuvmuuv@users.noreply.github.com>
2023-02-14 11:32:54 +0100

    Init

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,15 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.entergon.kiosk',
   appName: 'issue-capacitor-6297',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+  },
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, EMPTY } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  response: unknown;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .post('https://jsonplaceholder.typicode.com/posts', null, {
+        headers: {
+          something: 'special',
+        },
+      })
+      .pipe(
+        catchError((error) => {
+          console.error(error);
+
+          return EMPTY;
+        })
+      )
+      .subscribe((response) => {
+        console.log(response);
+
+        this.response = response;
+      });
+  }
 }
